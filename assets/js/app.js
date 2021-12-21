90d52e1fff2cd6061c84d23ad30b2d25f6b4b0db
joxiplayer <joxiplayer.id@gmail.com>
2021-12-22 01:33:49 +0700

    first commit

--- assets/js/app.js
+++ assets/js/app.js
@@ -94,3 +94,40 @@ searchMobileTrigger.addEventListener('click', () => {
 });
 
 
+function ShowToast() {
+    var sToast = document.getElementById('toast_container');
+    sToast.setAttribute('style', 'visibility:visible; animation:animation_toast 5s linear 1;');
+
+    var mToast = document.getElementById('toast_message');
+    mToast.innerHTML = "Halaman Ini Belum Tersedia";
+
+    setTimeout(HideToast, 5000);
+}
+
+function ShowToastTwo() {
+    var sToast = document.getElementById('toast_container');
+    sToast.setAttribute('style', 'visibility:visible; animation:animation_toast 5s linear 1;');
+
+    var mToast = document.getElementById('toast_message');
+    mToast.innerHTML = "Tampilan Ini Segera Hadir";
+
+    setTimeout(HideToast, 5000);
+}
+
+function ShowPremium() {
+    var sToast = document.getElementById('toast_container');
+    sToast.setAttribute('style', 'visibility:visible; animation:animation_toast 5s linear 1;');
+
+    var mToast = document.getElementById('toast_message');
+    mToast.innerHTML = "Paket Premium Belum Tersedia";
+
+    setTimeout(HideToast, 5000);
+}
+
+function HideToast() {
+    var sToast = document.getElementById('toast_container');
+    sToast.setAttribute('style', 'visibility:hidden; animation:animation_toast 5s linear 1;');
+
+    var mToast = document.getElementById('toast_message');
+    mToast.innerHTML = "This is a toast message";
+}
